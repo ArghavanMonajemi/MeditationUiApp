ce8139c1e3e3eec64c28e046f77b5d4400a3faae
Arghavan Monajemi <monajemi.am@gmail.com>
2024-06-16 18:03:33 +0330

    final Ui implemented.

--- app/src/main/java/com/example/meditationuiapp/HomeScreen.kt
+++ app/src/main/java/com/example/meditationuiapp/HomeScreen.kt
@@ -35,18 +35,8 @@ import androidx.compose.ui.graphics.Path
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.meditationuiapp.ui.theme.*
-import com.example.meditationuiapp.ui.theme.MeditationUiAppTheme
-
-@Preview
-@Composable
-fun HomeScreenPreview() {
-    MeditationUiAppTheme {
-        HomeScreen(name = "Arghavan")
-    }
-}
 
 @Composable
 fun HomeScreen(
